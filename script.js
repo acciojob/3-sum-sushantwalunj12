8ea76b64e1d6e13bb967444678dc85b0ad86ebbc
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-17 09:23:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function threeSum(arr, target) {
-  //your code here
+  nums.sort((a, b) => a - b);
+  let closestSum = Infinity;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+        closestSum = sum;
+      }
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return closestSum;
 }
 
 module.exports = threeSum;
